2453ee7e94d55a533d165409c06040a8eb48e5e2
mengyd <yidongmeng1992@gmail.com>
2019-01-24 11:07:15 +0100

    all

---
+++ contracts/Election.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.5.0;
+
+contract Election {
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
+	}
+
+	event votedEvent (
+		uint indexed _candidateId
+	);
+
+	mapping(uint => Candidate) public candidates;
+	mapping(address => bool) public voters;
+
+	uint public candidatesCount;
+	uint public voteTotal;
+
+	constructor() public {
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	}
+
+	function addCandidate (string memory _name) private {
+		candidatesCount ++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+	}
+
+	function vote (uint _candidateId) public {
+		require(!voters[msg.sender]);
+		require(_candidateId > 0 && _candidateId <= candidatesCount);
+		voters[msg.sender] = true;
+		candidates[_candidateId].voteCount ++;
+		voteTotal++;
+		emit votedEvent(_candidateId);
+	}
+
+}
